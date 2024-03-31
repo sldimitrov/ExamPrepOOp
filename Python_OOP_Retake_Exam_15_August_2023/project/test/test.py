3949c39dfe1b40e9f348368d1a39adf2a8830073
Slavi <slavidimitrov54@gmai.com>
2024-03-31 16:41:59 +0300

    Initialise a test dir

---
+++ Python_OOP_Retake_Exam_15_August_2023/project/test/test.py
@@ -0,0 +1,11 @@
+from project.trip import Trip
+from unittest import TestCase, main
+
+
+class TestTrip(TestCase):
+    def setUp(self):
+        trip = Trip()
+
+
+if __name__ == '__main__':
+    main()
